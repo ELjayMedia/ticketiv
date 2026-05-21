75753389414497ad3e466a1289131d1ea8851838
Lethukuthula Ndwandwe <150596482+ELjayMedia@users.noreply.github.com>
2026-05-21 21:29:29 +0200

    remove artists route

--- app/(public)/artists/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getPublicArtists } from "@/lib/data/public"
-import ArtistsClient from "./artists-client"
-
-export const dynamic = "force-dynamic"
-
-export default async function ArtistsPage() {
-  const artists = await getPublicArtists({ limit: 50 })
-  return <ArtistsClient initialArtists={artists} />
-}
